1db0c168e5f114484ed00d79b049ec8aed365a2c
sad.wang <sad912@foxmail.com>
2022-08-02 18:17:39 +0800

    feat:add test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+/// <reference types="vitest"/>
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),vueJsx({})]
+  plugins: [vue(), vueJsx({})],
+  test: {
+    // jest like test apis
+    globals: true,
+    // 模拟 dom 环境
+    environment: 'happy-dom',
+    // 支持 tsx
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  }
 })
